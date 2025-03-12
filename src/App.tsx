96b10544e093f3247e4bcfa3b97619a61406cf1d
lichangchang <15690847232@163.com>
2025-03-12 17:44:45 +0800

    测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 // import { useState } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/nav';
-import Home from './pages/home';
-import About from './pages/about';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Navbar from "./components/nav";
+import Home from "./pages/home";
+import About from "./pages/about";
 function App() {
-;
-
   return (
     <Router>
-    <div>
+      <div>
         {/* 顶部导航栏，保持不变 */}
-        <Navbar />
+        {/* <Navbar /> */}
 
         {/* 路由切换区域 */}
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
-    </div>
-</Router>
+      </div>
+    </Router>
   );
 }
 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,16 @@
-const Index =()=>{
+import { FloatButton } from "antd";
+import { UnorderedListOutlined } from "@ant-design/icons";
 
-
-    return (
-        <div>
-            博客首页
-            <p>欢迎来到我的博客！这里你可以找到各种有趣的文章。</p>
-            <ul>
-                <li>文章1</li>
-                <li>文章2</li>
-                <li>文章3</li>
-            </ul>
-        </div>
-    );
-}
-export default Index
+import "./index.css";
+const Index = () => {
+  return (
+    <div>
+      <FloatButton
+        icon={<UnorderedListOutlined />}
+        tooltip={<div>打开菜单</div>}
+      />
+      <h1 className="head">刚好的工具集合</h1>
+    </div>
+  );
+};
+export default Index;
